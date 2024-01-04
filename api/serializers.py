90f356e4575cb6baa82dfdeb234ecc813067f4a1
Anju <anjubijumon133@gmail.com>
2024-01-04 08:34:19 +0530

    3rd  message

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from todoapp.models import Todos
 
 class UserSerializers(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +9,10 @@ class UserSerializers(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data) # ** are provided for unpacking data
+
+class TodoSerilaizer(serializers.ModelSerializer):
+    user=serializers.StringRelatedField() #this code is to extract the username from user id while creating the todo task
+    class Meta:
+        model=Todos
+        fields="__all__"
+        read_only_fields=["id","user","status"]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.serializers import UserSerializers
+from api.serializers import UserSerializers,TodoSerilaizer
+from rest_framework.viewsets import ViewSet
+from rest_framework import permissions,authentication
+from todoapp.models import Todos
+from rest_framework import serializers
+
 # Create your views here.
 
 class RegistrationView(APIView):
@@ -14,5 +19,44 @@ class RegistrationView(APIView):
             return Response(data=serializer.data)
         else:
              return Response(data=serializer.errors)
+        
+#for all cruds   -- for performing all tasks 
+            
+class TodosView(ViewSet):
+
+    authentication_classes=[authentication.BasicAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+
+    def create(self,request,*args,**kwargs):
+        serializer=TodoSerilaizer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+        
+    def list(self,request,*args,**kwargs):
+        #list todos of logged-in user
+        qs=Todos.objects.filter(user=request.user)
+        serializer=TodoSerilaizer(qs,many=True)
+        return Response(data=serializer.data)
+    
+    def destroy(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        todo_object=Todos.objects.get(id=id)
+        if todo_object.user != request.user: #if condition check loged-in user same as todo user who created the todo task
+            raise serializers.ValidationError("permission denied for this user")
+        else:
+            todo_object.delete()
+            return Response(data={"message":"selected data deleted "})
+        
+    
+            
+
+
+
+
+        
+    # getb a todo detail
 
 
